9aed3e4ff588fee8788345f0fef3ffb2ed8bb2ff
seo-young <sy0hiu@gmail.com>
2024-01-13 18:52:43 +0900

    Chap7_13 경로 탐색(인접리스트)

---
+++ src/chap7/Chap7_13.java
@@ -0,0 +1,44 @@
+package chap7;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Chap7_13 {
+
+    static int n, m, answer=0;
+    static int[] ch;
+    static ArrayList<ArrayList<Integer>> graph;
+
+    public void DFS(int v) {
+        if(v==n) answer++;
+        else {
+            for(int nv : graph.get(v)) {
+                if(ch[nv]==0) {
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Chap7_12 T = new Chap7_12();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>(); //객체 생성
+        for(int i=0; i<=n; i++) {
+            graph.add(new ArrayList<Integer>()); //객체 생성
+        }
+        ch = new int[n+1];
+        for(int i=0; i<m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph.get(a).add(b);
+        }
+        ch[1]=1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
