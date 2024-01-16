aa7e9cd3b3c5b7718c42a480de51f9a4b68a5a93
seo-young <sy0hiu@gmail.com>
2024-01-17 00:31:25 +0900

    Chap8_01 합이 같은 부분집합

--- src/chap8/Chap8_01.java
+++ src/chap8/Chap8_01.java
@@ -6,27 +6,24 @@ import java.util.Scanner;
 public class Chap8_01 {
 
     static String answer = "NO";
-    static int sum=0;
     static int total=0;
     static int n;
     static int[] arr;
-    static int[] ch;
+    boolean flag = false;
 
-    public String DFS(int v) {
+    public void DFS(int v, int sum) {
+        if(flag) return;
+        if(sum>total/2) return;
         if(v==n) {
-            for(int i=0; i<v; i++) {
-                if(ch[i]==1) sum += arr[i];
+            if((total-sum)==sum) {
+                answer = "YES";
+                flag = true;
             }
-            if((total-sum)==sum) answer = "YES";
-            sum=0;
         }
         else{
-            ch[v]=1;
-            DFS(v+1);
-            ch[v]=0;
-            DFS(v+1);
+            DFS(v+1, sum+arr[v]);
+            DFS(v+1, sum);
         }
-        return answer;
     }
 
     public static void main(String[] args) {
@@ -34,11 +31,11 @@ public class Chap8_01 {
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
         arr = new int[n];
-        ch = new int[n];
         for(int i=0; i<n; i++) {
             arr[i]=kb.nextInt();
             total += arr[i];
         }
-        System.out.println(T.DFS(0));
+        T.DFS(0, 0);
+        System.out.println(answer);
     }
 }
