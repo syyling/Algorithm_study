73da9490c16b907baea2e0cc12be1d7428dd32c6
SeoYoLIng <102759992+syyling@users.noreply.github.com>
2024-01-20 02:17:58 +0900

    Chap8_04 중복순열

---
+++ src/chap8/Chap8_04.java
@@ -0,0 +1,31 @@
+package chap8;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int n, m;
+    static int[] pm;
+
+    public void DFS(int L) {
+        if(L==m) {
+            for(int x: pm) System.out.print(x + " ");
+            System.out.println();
+        }
+        else {
+            for(int i=1; i<=n; i++) {
+                pm[L]=i;
+                DFS(L+1);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        pm = new int[m];
+        T.DFS(0);
+    }
+}
